0c22fd4b63816e92d6ddd5f9853af7dbb5ccee01
baljeet <baljeet32kumar@gmail.com>
2022-06-03 14:55:50 +0530

    Creation of  Starting Templates

--- myVillageApp/villageapp/urls.py
+++ myVillageApp/villageapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from villageapp import views
 urlpatterns = [
-    path('',views.home, name="village-home" ),
+    path('', views.home, name="village-home"),
+    path('about/', views.about, name="village-about")
 ]
--- myVillageApp/villageapp/views.py
+++ myVillageApp/villageapp/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+complaints = [
+
+    {
+        'author': 'XYZ',
+        'title': 'ABc Cde',
+        'Desc': 'sghsgd  fjshfj  sjfhdkjs  shfkjs ',
+        'date-posted': 'hjsdkjfh  shfkf'
+
+    },
+    {
+        'author': 'XYZ',
+        'title': 'ABc Cde',
+        'Desc': 'sghsgd  fjshfj  sjfhdkjs  shfkjs ',
+        'date-posted': 'hjsdkjfh  shfkf'
+
+    },
+    {
+        'author': 'XYZ',
+        'title': 'ABc Cde',
+        'Desc': 'sghsgd  fjshfj  sjfhdkjs  shfkjs ',
+        'dateposted': 'hjsdkjfh  shfkf'
+    }
+]
+
 
 def home(req):
-    return HttpResponse("Testing the setup")
+   
+    return render(req, 'villageapp/index.html',{'complaints':complaints})
+
+
+def about(req):
+    return render(req, 'villageapp/about.html')
